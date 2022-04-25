ab6629feeaa67f849d7b7bda7f842400a4d6d4d3
rothberry <phil.roth077@gmail.com>
2022-04-25 14:01:55 -0700

    fix routes

--- rails-payers/config/routes.rb
+++ rails-payers/config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # resources :transactions
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show, :index]
   resources :payers, only: [:create, :show, :index]
+  resources :transactions, only: [:show, :index]
   post '/spend_points', to: "transactions#spend_points"
   post '/add_transaction', to: "transactions#add_transaction"
 
