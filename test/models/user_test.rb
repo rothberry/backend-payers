dab7ca89e1b31929bfe5ae46a2b0e4b9d30ff14c
phil <phil.roth077@gmail.com>
2022-04-26 13:31:30 -0700

    start testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  puts "TESTING USERS..."
+  test "user created successfully" do
+    user = User.new(name: "Phil")
+    assert user.save
+  end
+
+  test "second" do
+    puts "second"
+  end
+
+  test "TESTING" do 
+    puts "third"
+    p User.all
+    debugger
+  end
+
 end
