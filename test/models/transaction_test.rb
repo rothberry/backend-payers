afc95ef9bdd5ea6160367c00a7ff5caef38a9af5
rothberry <phil.roth077@gmail.com>
2022-04-27 10:31:13 -0700

    more testing

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -4,4 +4,7 @@ class TransactionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,19 +5,38 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   puts "TESTING USERS..."
+  setup :setup_users
+  # teardown :remove_users
+
   test "user created successfully" do
     user = User.new(name: "Phil")
     assert user.save
   end
 
-  test "second" do
-    puts "second"
+  test "user needs a name" do
+    assert_not users(:bad_1).save
   end
 
-  test "TESTING" do 
-    puts "third"
-    p User.all
-    debugger
+  test "has a user" do
+    # p @phil
+    assert @phil
+  end
+
+  test "user has many transactions" do
+    # debugger
+    assert @phil.class == Transaction::ActiveRecord_Associations_CollectionProxy
+  end
+  
+  private 
+  
+  def setup_users
+    @phil = users(:phil)
+    @jill = users(:jill)
+  end
+
+  def remove_users
+    @phil = nil
+    @jill = nil
   end
 
 end
